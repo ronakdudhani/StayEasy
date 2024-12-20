6cc380c37b02839e1e6d00e3f30375f64e2e0133
ronakdudhani <ronakdudhani@gmail.com>
2024-12-20 12:00:00 +0530

    Tested and fixed booking issues

--- models.py
+++ models.py
@@ -1,2 +1,3 @@
 # Changes made on 2024-12-12
 # Changes made on 2024-12-13
+# Changes made on 2024-12-20
--- views.py
+++ views.py
@@ -2,3 +2,4 @@
 # Changes made on 2024-12-16
 # Changes made on 2024-12-17
 # Changes made on 2024-12-19
+# Changes made on 2024-12-20
